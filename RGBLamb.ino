61b139adb2254e548ca8b428d11ded6df7a830cd
Fabian H <fabian.h001@gmail.com>
2019-05-12 20:42:50 +1000

    merged with disco mode

--- RGBLamb.ino
+++ RGBLamb.ino
@@ -1,9 +1,87 @@
-#include <Adafruit_NeoPixel.h>
-#ifdef __AVR__
-  #include <avr/power.h>
-#endif
+#include <FastLED.h> //for disco mode
+#include <Adafruit_NeoPixel.h> //other modes
 
-#define PIN 5
+
+//The amount of LEDs in the setup
+#define NUM_LEDS 144
+
+//The pin that controls the LEDs for Disco (FastLED)
+#define LED_PIN 1 // = D1 since ESP is used for the project
+//The pin that is used for other modes. 
+#define PIN_GPIO 5 // GPIO05 = D1. But in this case we care about GPIO pin
+//Arduino loop delay
+#define DELAY 1
+
+//variables for DISCO
+//The pin that we read sensor values form
+#define ANALOG_READ 0
+
+//Confirmed microphone low value, and max value
+#define MIC_LOW 0.0
+#define MIC_HIGH 1024.0
+/** Other macros */
+//How many previous sensor values effects the operating average?
+#define AVGLEN 15
+//How many previous sensor values decides if we are on a peak/HIGH (e.g. in a song)
+#define LONG_SECTOR 5
+
+//Mneumonics
+#define HIGH  3
+#define NORMAL 2
+
+//How long do we keep the "current average" sound, before restarting the measuring
+#define MSECS 2000
+#define CYCLES MSECS / DELAY
+
+/*Sometimes readings are wrong or strange. How much is a reading allowed
+to deviate from the average to not be discarded? **/
+#define DEV_THRESH 0.8
+
+float fscale( float originalMin, float originalMax, float newBegin, float newEnd, float inputValue, float curve);
+void insert(int val, int *avgs, int len);
+int compute_average(int *avgs, int len);
+void visualize_music();
+
+//How many LEDs to we display
+int curshow = NUM_LEDS;
+
+//Showing different colors based on the mode.
+int songmode = NORMAL;
+
+//Average sound measurement the last CYCLES
+unsigned long song_avg;
+
+//The amount of iterations since the song_avg was reset
+int iter = 0;
+
+//The speed the LEDs fade to black if not relit
+float fade_scale = 1.2;
+
+//Led array
+CRGB leds[NUM_LEDS];
+
+/*Short sound avg used to "normalize" the input values.
+We use the short average instead of using the sensor input directly */
+int avgs[AVGLEN] = {-1};
+
+//Longer sound avg
+int long_avg[LONG_SECTOR] = {-1};
+
+//Keeping track how often, and how long times we hit a certain mode
+struct time_keeping {
+  unsigned long times_start;
+  short times;
+};
+
+//How much to increment or decrement each color every cycle
+struct color {
+  int r;
+  int g;
+  int b;
+};
+
+struct time_keeping high;
+struct color Color; 
 
 // Parameter 1 = number of pixels in strip
 // Parameter 2 = Arduino pin number (most are valid)
@@ -13,7 +91,7 @@
 //   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
 //   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(144, PIN, NEO_RGB + NEO_KHZ800);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, PIN_GPIO, NEO_RGB + NEO_KHZ800);
 
 // IMPORTANT: To reduce NeoPixel burnout risk, add 1000 uF capacitor across
 // pixel power leads, add 300 - 500 Ohm resistor on first pixel's data input
@@ -21,33 +99,38 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(144, PIN, NEO_RGB + NEO_KHZ800);
 // on a live circuit...if you must, connect GND first.
 
 //button
-int pushCounter;
+//mode for program
+int mode = 0;
+//GPIO pin button is attached to
 const int pushButton = 12;
 
-//variables for disco
-int vol = 0;
-float total = 0;
-int fadeCol = 0;
-int val[25];
-int volLast = 0;
-int fadeAmt = 0;
-int counter = 0;
-//fadeThickness = 0;  // CHANGE THICKNESS (0-10)
 
 void setup() {
-  // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
-  #if defined (__AVR_ATtiny85__)
-    if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
-  #endif
-  // End of trinket special code
-
   strip.begin();
   strip.setBrightness(50);
   strip.show(); // Initialize all pixels to 'off'
-
-  pushCounter = 0; // set lamp mode to 0
-
   Serial.begin(9600);
+  
+  //disco 
+  FastLED.addLeds<NEOPIXEL, LED_PIN>(leds, NUM_LEDS);
+  //FastLED.setMaxPowerInVoltsAndMilliamps(5,7000);
+  FastLED.setBrightness(50); 
+  for (int i = 0; i < NUM_LEDS; i++) 
+    leds[i] = CRGB(0, 0, 255);
+  FastLED.show(); 
+  delay(5000);
+
+  //bootstrap average with some low values
+  for (int i = 0; i < AVGLEN; i++) {  
+    insert(250, avgs, AVGLEN);
+  }
+
+  //Initial values
+  high.times = 0;
+  high.times_start = millis();
+  Color.r = 0;  
+  Color.g = 0;
+  Color.b = 1;  
 }
 
 void loop() {
@@ -57,10 +140,10 @@ void loop() {
     // 1 = cold white light 
     // 2 =  warm white light
     // 3 = disco mode
-    if(pushCounter >= 3){
-      pushCounter = 0;
+    if(mode >= 3){
+      mode = 0;
     } else {
-      pushCounter++;
+      mode++;
     }
     delay(500);
   }*/
@@ -68,7 +151,7 @@ void loop() {
   //Serial.print("Push counter at ");
   //Serial.println(pushCounter);
   
-  switch (pushCounter) {
+  switch (mode) {
     case 0: //rainbow
       rainbowCycle(20);
       break;
@@ -79,25 +162,12 @@ void loop() {
       lightOn(strip.Color(255, 103, 23));
       break;
     case 3: //disco
-      disco();
+      visualize_music();
       break;
     default:
       break;
   } 
-    // Some example procedures showing how to display to the pixels:
-    //colorWipe(strip.Color(255, 0, 0), 50); // Red
-    //colorWipe(strip.Color(0, 255, 0), 50); // Green
-    //colorWipe(strip.Color(0, 0, 255), 50); // Blue
-    //colorWipe(strip.Color(0, 0, 0, 255), 50); // White RGBW
-    // Send a theater pixel chase in...
-    //theaterChase(strip.Color(127, 127, 127), 50); // White
-    //theaterChase(strip.Color(127, 0, 0), 50); // Red
-    //theaterChase(strip.Color(0, 0, 127), 50); // Blue
-  
-    //rainbow(20);
-    //rainbowCycle(20);
-    //theaterChaseRainbow(50);
-    
+  delay(DELAY);       // delay in between reads for stability   
 }
 
 
@@ -107,19 +177,6 @@ void lightOn(uint32_t color){
         strip.show();
     }  
 }
-
-
-void disco() {
-  
-      vol = analogRead(A0);
-      
-      Serial.println(vol);
-      delay(50);
-  
-}
-
-
-
 // Fill the dots one after the other with a color
 void colorWipe(uint32_t c, uint8_t wait) {
   for(uint16_t i=0; i<strip.numPixels(); i++) {
@@ -203,4 +260,207 @@ uint32_t Wheel(byte WheelPos) {
   }
   WheelPos -= 170;
   return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+}
+
+
+/**Funtion to check if the lamp should either enter a HIGH mode,
+or revert to NORMAL if already in HIGH. If the sensors report values
+that are higher than 1.1 times the average values, and this has happened
+more than 30 times the last few milliseconds, it will enter HIGH mode. 
+TODO: Not very well written, remove hardcoded values, and make it more
+reusable and configurable.  */
+void check_high(int avg) {
+  if (avg > (song_avg/iter * 1.1))  {
+    if (high.times != 0) {
+      if (millis() - high.times_start > 200.0) {
+        high.times = 0;
+        songmode = NORMAL;
+      } else {
+        high.times_start = millis();  
+        high.times++; 
+      }
+    } else {
+      high.times++;
+      high.times_start = millis();
+
+    }
+  }
+  if (high.times > 30 && millis() - high.times_start < 50.0)
+    songmode = HIGH;
+  else if (millis() - high.times_start > 200) {
+    high.times = 0;
+    songmode = NORMAL;
+  }
+}
+
+//Main function for visualizing the sounds in the lamp
+void visualize_music() {
+  int sensor_value, mapped, avg, longavg;
+  
+  //Actual sensor value
+  sensor_value = analogRead(ANALOG_READ);
+  
+  //If 0, discard immediately. Probably not right and save CPU.
+  if (sensor_value == 0)
+    return;
+
+  //Discard readings that deviates too much from the past avg.
+  mapped = (float)fscale(MIC_LOW, MIC_HIGH, MIC_LOW, (float)MIC_HIGH, (float)sensor_value, 2.0);
+  avg = compute_average(avgs, AVGLEN);
+
+  if (((avg - mapped) > avg*DEV_THRESH)) //|| ((avg - mapped) < -avg*DEV_THRESH))
+    return;
+  
+  //Insert new avg. values
+  insert(mapped, avgs, AVGLEN); 
+  insert(avg, long_avg, LONG_SECTOR); 
+
+  //Compute the "song average" sensor value
+  song_avg += avg;
+  iter++;
+  if (iter > CYCLES) {  
+    song_avg = song_avg / iter;
+    iter = 1;
+  }
+    
+  longavg = compute_average(long_avg, LONG_SECTOR);
+
+  //Check if we enter HIGH mode 
+  check_high(longavg);  
+
+  if (songmode == HIGH) {
+    fade_scale = 3;
+    Color.r =  8;
+    Color.g = 1;
+    Color.b = -2;
+  }
+  else if (songmode == NORMAL) {
+    fade_scale = 3;
+    Color.r = -1;
+    Color.b = 6;
+    Color.g = -2;
+  }
+
+  //Decides how many of the LEDs will be lit
+  curshow = fscale(MIC_LOW, MIC_HIGH, 0.0, (float)NUM_LEDS, (float)avg, -1);
+
+  /*Set the different leds. Control for too high and too low values.
+          Fun thing to try: Dont account for overflow in one direction, 
+    some interesting light effects appear! */
+  for (int i = 0; i < NUM_LEDS; i++) 
+    //The leds we want to show
+    if (i < curshow) {
+      if (leds[i].r + Color.r > 255)
+        leds[i].r = 255;
+      else if (leds[i].r + Color.r < 0)
+        leds[i].r = 0;
+      else
+        leds[i].r = leds[i].r + Color.r;
+          
+      if (leds[i].g + Color.g > 255)
+        leds[i].g = 255;
+      else if (leds[i].g + Color.g < 0)
+        leds[i].g = 0;
+      else 
+        leds[i].g = leds[i].g + Color.g;
+
+      if (leds[i].b + Color.b > 255)
+        leds[i].b = 255;
+      else if (leds[i].b + Color.b < 0)
+        leds[i].b = 0;
+      else 
+        leds[i].b = leds[i].b + Color.b;  
+      
+    //All the other LEDs begin their fading journey to eventual total darkness
+    } else {
+      leds[i] = CRGB(leds[i].r/fade_scale, leds[i].g/fade_scale, leds[i].b/fade_scale);
+    }
+  FastLED.show(); 
+}
+//Compute average of a int array, given the starting pointer and the length
+int compute_average(int *avgs, int len) {
+  int sum = 0;
+  for (int i = 0; i < len; i++)
+    sum += avgs[i];
+
+  return (int)(sum / len);
+
+}
+
+//Insert a value into an array, and shift it down removing
+//the first value if array already full 
+void insert(int val, int *avgs, int len) {
+  for (int i = 0; i < len; i++) {
+    if (avgs[i] == -1) {
+      avgs[i] = val;
+      return;
+    }  
+  }
+
+  for (int i = 1; i < len; i++) {
+    avgs[i - 1] = avgs[i];
+  }
+  avgs[len - 1] = val;
+}
+
+//Function imported from the arduino website.
+//Basically map, but with a curve on the scale (can be non-uniform).
+float fscale( float originalMin, float originalMax, float newBegin, float
+    newEnd, float inputValue, float curve){
+
+  float OriginalRange = 0;
+  float NewRange = 0;
+  float zeroRefCurVal = 0;
+  float normalizedCurVal = 0;
+  float rangedValue = 0;
+  boolean invFlag = 0;
+
+
+  // condition curve parameter
+  // limit range
+
+  if (curve > 10) curve = 10;
+  if (curve < -10) curve = -10;
+
+  curve = (curve * -.1) ; // - invert and scale - this seems more intuitive - postive numbers give more weight to high end on output 
+  curve = pow(10, curve); // convert linear scale into lograthimic exponent for other pow function
+
+  // Check for out of range inputValues
+  if (inputValue < originalMin) {
+    inputValue = originalMin;
+  }
+  if (inputValue > originalMax) {
+    inputValue = originalMax;
+  }
+
+  // Zero Refference the values
+  OriginalRange = originalMax - originalMin;
+
+  if (newEnd > newBegin){ 
+    NewRange = newEnd - newBegin;
+  }
+  else
+  {
+    NewRange = newBegin - newEnd; 
+    invFlag = 1;
+  }
+
+  zeroRefCurVal = inputValue - originalMin;
+  normalizedCurVal  =  zeroRefCurVal / OriginalRange;   // normalize to 0 - 1 float
+
+  // Check for originalMin > originalMax  - the math for all other cases i.e. negative numbers seems to work out fine 
+  if (originalMin > originalMax ) {
+    return 0;
+  }
+
+  if (invFlag == 0){
+    rangedValue =  (pow(normalizedCurVal, curve) * NewRange) + newBegin;
+
+  }
+  else     // invert the ranges
+  {   
+    rangedValue =  newBegin - (pow(normalizedCurVal, curve) * NewRange); 
+  }
+
+  return rangedValue;
 }
